5ce688e6ea011a3294b9caaaf37e5ec528200f7e
IGORAOLIV <igoralmeida0105@gmail.com>
2023-05-03 14:02:25 -0300

    First commit Desafio

--- desafio-calculadora-viagem.js
+++ desafio-calculadora-viagem.js
@@ -1,11 +1,11 @@
 // Define as variáveis
-var precoCombustivel = 4.50; // Preço por litro
-var consumoCarro = 12; // Quilômetros por litro
-var distanciaViagem = 200; // Quilômetros
+let precoCombustivel = 4.50; // Preço por litro
+let consumoCarro = 12; // Quilômetros por litro
+let distanciaViagem = 200; // Quilômetros
 
 // Calcula o valor gasto em combustível
-var litrosConsumidos = distanciaViagem / consumoCarro;
-var valorGasto = litrosConsumidos * precoCombustivel;
+let litrosConsumidos = distanciaViagem / consumoCarro;
+let valorGasto = litrosConsumidos * precoCombustivel;
 
 // Imprime o resultado
 console.log("O valor gasto em combustível será de R$" + valorGasto.toFixed(2));
